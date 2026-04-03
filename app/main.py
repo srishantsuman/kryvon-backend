e4268c03fb01c1818bab63dfff2f1df1fbe86ab5
srishantsuman <srishantsuman067@gmail.com>
2026-04-03 13:56:29 +0530

    fix cors properly

--- app/main.py
+++ app/main.py
@@ -34,12 +34,17 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://kryvon.in",
+        "https://www.kryvon.in",
+        "https://kryvon-frontend.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(api_router)
 
 
